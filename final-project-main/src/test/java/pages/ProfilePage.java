4e1dfa524d207ba52ae5e3146752b636d24f3dab
kilelka <kogan.oa@mail.ru>
2025-05-18 14:56:02 +0300

    Начальный коммит: итоговый проект

--- final-project-main/src/test/java/pages/ProfilePage.java
+++ final-project-main/src/test/java/pages/ProfilePage.java
@@ -1,36 +1,36 @@
 package pages;
 
-import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$x;
+import static java.time.Duration.ofSeconds;
 
 public class ProfilePage {
 
-    private final SelenideElement aboutSectionContainer = $(".text-field_cnt");
-    private final SelenideElement aboutTextarea = $(".text-field_cnt textarea");
-    private final SelenideElement saveButton = $x("//*[@id='hook_Block_AboutUserRB']//button[contains(text(), 'Сохранить')]");
-    private final SelenideElement aboutTextBlock = $("[tsid='TextFieldText']");
+    private static final By ABOUT_SECTION_CONTAINER = By.cssSelector(".text-field_cnt");
+    private static final By ABOUT_TEXTAREA = By.cssSelector(".text-field_cnt textarea");
+    private static final By SAVE_BUTTON = By.xpath("//*[@id='hook_Block_AboutUserRB']//button[contains(text(), 'Сохранить')]");
+    private static final By ABOUT_TEXT_BLOCK = By.cssSelector("[tsid='TextFieldText']");
 
     public ProfilePage openEditAboutSection() {
-        aboutSectionContainer.shouldBe(visible).click();
-        aboutTextarea.shouldBe(visible);
+        $(ABOUT_SECTION_CONTAINER).shouldBe(visible, ofSeconds(15)).click();
+        $(ABOUT_TEXTAREA).shouldBe(visible, ofSeconds(15));
         return this;
     }
 
     public ProfilePage updateAboutMe(String text) {
-        aboutTextarea.clear();
-        aboutTextarea.setValue(text);
+        $(ABOUT_TEXTAREA).clear();
+        $(ABOUT_TEXTAREA).setValue(text);
         return this;
     }
 
     public ProfilePage saveChanges() {
-        saveButton.shouldBe(visible).click();
+        $(SAVE_BUTTON).shouldBe(visible).click();
         return this;
     }
 
     public String getAboutMeText() {
-        return aboutTextBlock.shouldBe(visible).getText();
+        return $(ABOUT_TEXT_BLOCK).shouldBe(visible).getText();
     }
 }
